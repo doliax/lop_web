aa05d89b553dd4e89ad597de9f12066424487202
doliax <ramon.moncholi08@gmail.com>
2023-11-16 10:31:11 +0100

    conexion jugadas backend al frontend 100%

--- lop_frontend/src/App.jsx
+++ lop_frontend/src/App.jsx
@@ -1,13 +1,22 @@
 
 import './App.css';
 import { PaginaPrincipal } from './components/PaginaPrincipal';
+import { QueryClient, QueryClientProvider, useQuery } from 'react-query'
+
 
 function App() {
-  return (
-    <div className="text-center">
-     <PaginaPrincipal/>
-    </div>
-  );
-}
+
+  const queryClient = new QueryClient()
+  
+
+     return (
+       <QueryClientProvider client={queryClient}>
+<div className="text-center">
+<PaginaPrincipal/>
+</div>
+       </QueryClientProvider>
+     )
+  }
 
 export default App;
+
--- lop_frontend/src/components/JugadasImportantes.jsx
+++ lop_frontend/src/components/JugadasImportantes.jsx
@@ -1,31 +1,71 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { useQuery } from 'react-query';
+import axios from 'axios';
 
 export const JugadasImportantes = () => {
 
-   //Declaración del objeto (para testear antes del backend)
-    const jugadas = [
-        {
-        id: 1,
-        src: 'https://www.youtube.com/embed/xAFmRdSSyb8?autoplay=1&mute=0',
-        titulo: 'PDD Rumble'
-        },
-        {
-            id: 2,
-            src: 'https://www.youtube.com/embed/ZPCfoCVCx3U?autoplay=1&mute=0',
-            titulo: 'Zed Jugada Legendaria'
-            },
-]
 
-    //Cards con las jugadas
-  return (
+  const baseURL = "http://localhost:3000/jugadas";
+
+  const [post, setPost] = React.useState(null);
+//Hay que poner los datos en el setPost mediante el useEffect!!!!!!!!
+  React.useEffect(() => {
+    axios.get(baseURL)
+      .then((response) => {
+        setPost(response.data);
+      })
+      .catch((error) => {
+        console.error('Error al obtener datos:', error);
+      });
+  }, []);
+  
+  React.useEffect(() => {
+    console.log(post);
+  }, [post]);
+  
+  {/*const { data: jugadas, isLoading } = useQuery('jugadas', async () => {
+    const response = await axios.get('http://localhost:3000/jugadas');
+    return response.data;
+  });
+
+  useEffect(() => {
+    // Puedes realizar acciones adicionales cuando cambian los datos, si es necesario
+    console.log('Datos actualizados:', jugadas);
+  }, [jugadas]);
+
+  if (isLoading) {
+    return <div>Cargando...</div>;
+  }
+
+ return (
     <div className='m-0 inline-flex pt-20'>
-        {/*Recorro el array de objetos  */}
-        {jugadas.map((x) => (
-        <div key={x.id} className='pl-14'>
-         <iframe className='h-96 w-96' src={x.src} title={x.titulo} frameBorder="0" allowFullScreen="allowfullscreen"></iframe>
+      <h2>Test</h2>
+      {jugadas.map((x) => (
+        <div key={x._id} className='pl-14'>
+          <iframe className='h-96 w-96' src={x.src} title={x.titulo} frameBorder="0" allowFullScreen="allowfullscreen"></iframe>
           <h3 className='font-thin text-xl mt-3'>{x.titulo}</h3>
         </div>
       ))}
     </div>
-  )
-}
+  );
+*/}
+
+
+return (
+  <div className='m-0 inline-flex pt-20'>
+    {post ? (
+      post.map((x) => (
+        <div key={x._id} className='pl-14'>
+          <iframe className='h-96 w-96' src={x.video} title={x.nombre} frameBorder="0" allowFullScreen="allowfullscreen"></iframe>
+          <h3 className='font-thin text-xl mt-3'>{x.nombre}</h3>
+        </div>
+      ))
+    ) : (
+      <div>Cargando...</div>
+    )}
+  </div>
+);
+
+  
+ 
+};
